aeba42f0c4ba2b2a1fdd855e0107de2cdcbc26e5
Peter Aigner <mastapetz@icloud.com>
2022-06-09 22:29:10 +0200

    advanced finished for single entry

--- Peter/02advanced/script.js
+++ Peter/02advanced/script.js
@@ -1,5 +1,4 @@
 "use strict";
-var _a;
 class vehicles {
     constructor(image, maker, model, buildYear, kilometers, price, PS, numberOfSeats, fuelType) {
         this.image = image;
@@ -13,8 +12,7 @@ class vehicles {
         this.fuelType = fuelType;
     }
     carInfo() {
-        return;
-        `    <div class="card">
+        return `    <div class="card">
         <div class="card-header d-flex justify-content-between bg-transparent">
           <div>${this.model} by ${this.maker} </div>
         </div>
@@ -22,20 +20,24 @@ class vehicles {
         <div class="card-body">
           <div class="card-title text-center h5">${this.model}</div><br>
           <hr>
-          <div class="card-text">${this.kilometers}</div>
+          <div class="card-text">${this.kilometers} km</div>
           <div class="card-text">numberOfSeats Type: ${this.numberOfSeats}  </div>
           <div class="card-text">Fuel Type: ${this.fuelType}  </div>
           <div class="card-text">PS: ${this.PS}  </div>
 
         </div>
-        <div class="card-footer bg-transparent text-center">
-        <div class="card-text">Price: ${this.price}  </div>
-          
-          </div>
+        <p>
+            <button class="btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePricecars"
+                aria-expanded="false" aria-controls="collapsePricecars">Show Price
+            </button>
+        </p>
+        <div class="collapse" id="collapsePricecars">
+            <div class="card card-body">Price: ${this.price}</div>
+        </div>
     </div> `;
     }
 }
-let ente = new vehicles("#", "Citroën", "2CV", 1988, 99000, 7000, 40, 4, "normal");
+let ente = new vehicles("./02advanced/vehicles/2CV.jpg", "Citroën", "2CV", 1988, 99000, 7000, 40, 4, "normal");
 class trucks extends vehicles {
     constructor(image, maker, model, buildYear, kilometers, price, PS, numberOfSeats, fuelType, bLicenceSuff, bodyType, maxLoadWeight, emptyWeight) {
         super(image, maker, model, buildYear, kilometers, price, PS, numberOfSeats, fuelType);
@@ -45,32 +47,40 @@ class trucks extends vehicles {
         this.emptyWeight = emptyWeight;
     }
     truckInfo() {
-        return;
-        `    <div class="card">
-        <div class="card-header d-flex justify-content-between bg-transparent">
-          <div>${this.model} by ${this.maker} </div>
+        return `    <div class=" card">
+    <div class="card-header d-flex justify-content-between bg-transparent">
+        ${this.model} by ${this.maker}
+    </div>
+    <img src="${this.image}" class="card-img-top img-fluid" alt="...">
+    <div class="card-body">
+        <div class="card-title text-center h5">${this.model}<br> ${this.bodyType}
         </div>
-        <img src="${this.image}" class="card-img-top img-fluid" alt="...">
-        <div class="card-body">
-          <div class="card-title text-center h5">${this.model}<br> ${this.bodyType} </div><br>
-          <hr>
-          <div class="card-text">${this.kilometers}</div>
-          <div class="card-text">B License enough: ${this.bLicenceSuff}<br>
-          Empty Weight: ${this.emptyWeight}<br>
-          Max Load Weight: ${this.maxLoadWeight} </div>
-          <div class="card-text">numberOfSeats Type: ${this.numberOfSeats}  </div>
-          <div class="card-text">Fuel Type: ${this.fuelType}  </div>
-          <div class="card-text">PS: ${this.PS}  </div>
+        <hr>
+        <div class="card-text">
+            ${this.kilometers} km <br>
+            B License enough: ${this.bLicenceSuff}<br>
+            Empty Weight: ${this.emptyWeight}<br>
+            Max Load Weight: ${this.maxLoadWeight} <br>
+            Seats: ${this.numberOfSeats}
+            Fuel Type: ${this.fuelType} <br>
+            PS: ${this.PS}
+        </div>
 
+
+        <p>
+            <button class="btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePriceTrucks"
+                aria-expanded="false" aria-controls="collapsePriceTrucks">Show Price
+            </button>
+        </p>
+        <div class="collapse" id="collapsePriceTrucks">
+            <div class="card card-body">Price: ${this.price}</div>
         </div>
-        <div class="card-footer bg-transparent text-center">
-        <div class="card-text">Price: ${this.price}  </div>
-          
-          </div>
-    </div> `;
+    </div>
+             `;
     }
 }
-let ram = new trucks("#", "Chrysler", "Dodge Ram", 2022, 0, 54000, 308, 6, "normal", true, "Pickup Quad-Cab", 8000, 34000);
+// <div class="card-footer bg-transparent text-center"> (for line 77) </div> (for line 76)
+let ram = new trucks("./02advanced/vehicles/RAM.JPG", "Chrysler", "Dodge Ram", 2022, 0, 54000, 308, 6, "normal", true, "Pickup Quad-Cab", 8000, 34000);
 class motorBikes extends vehicles {
     constructor(image, maker, model, buildYear, kilometers, price, PS, numberOfSeats, fuelType, ageLimit, canDriveWithB) {
         super(image, maker, model, buildYear, kilometers, price, PS, numberOfSeats, fuelType);
@@ -78,8 +88,7 @@ class motorBikes extends vehicles {
         this.canDriveWithB = canDriveWithB;
     }
     bikeInfo() {
-        return;
-        `    <div class="card">
+        return `    <div class="card">
         <div class="card-header d-flex justify-content-between bg-transparent">
           <div>${this.model} by ${this.maker} </div>
         </div>
@@ -87,7 +96,7 @@ class motorBikes extends vehicles {
         <div class="card-body">
           <div class="card-title text-center h5">${this.model}</div><br>
           <hr>
-          <div class="card-text">${this.kilometers}</div>
+          <div class="card-text">${this.kilometers} km </div>
           <div class="card-text">Can drive with B: ${this.canDriveWithB}  </div>
           <div class="card-text">Has age limit: ${this.ageLimit}  </div>
 
@@ -95,11 +104,26 @@ class motorBikes extends vehicles {
           <div class="card-text">PS: ${this.PS}  </div>
 
         </div>
-        <div class="card-footer bg-transparent text-center">
-        <div class="card-text">Price: ${this.price}  </div>
+        <p>
+            <button class="btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePriceBike"
+                aria-expanded="false" aria-controls="collapsePriceBike">Show Price
+            </button>
+        </p>
+        <div class="collapse" id="collapsePriceBike">
+            <div class="card card-body">Price: ${this.price}</div>
+        </div>
           
           </div>
     </div> `;
     }
 }
-let mofaMaxi = new motorBikes("#", "Steyr Daimler Puch", "N2", 1992, 20000, 10000, 1.2, 1, "normal", false, true)((_a = document.querySelector(("ouput-cars"))) === null || _a === void 0 ? void 0 : _a.innerHTML);
+let mofaMaxi = new motorBikes("./02advanced/vehicles/n2.jpg", "Steyr Daimler Puch", "N2", 1992, 20000, 10000, 1.2, 1, "normal", false, true);
+// output cards
+var outputEnte = document.getElementById("ouput-cars");
+outputEnte.innerHTML = ente.carInfo();
+// output trucks
+var outputRam = document.getElementById("ouput-trucks");
+outputRam.innerHTML += ram.truckInfo();
+// output bikes
+var outputPuch = document.getElementById("ouput-bikes");
+outputPuch.innerHTML += mofaMaxi.bikeInfo();
--- Peter/02advanced/script.ts
+++ Peter/02advanced/script.ts
@@ -6,7 +6,7 @@ class vehicles {
   kilometers: number;
   price: number;
   PS: number;
-  numberOfSeats?: number;
+  numberOfSeats: number;
   fuelType: string;
   constructor(image: string, maker: string, model: string, buildYear: number, kilometers: number, price: number, PS: number, numberOfSeats: number, fuelType: string) {
     this.image = image;
@@ -20,8 +20,7 @@ class vehicles {
     this.fuelType = fuelType;
   }
   carInfo() {
-    return
-    `    <div class="card">
+    return    `    <div class="card">
         <div class="card-header d-flex justify-content-between bg-transparent">
           <div>${this.model} by ${this.maker} </div>
         </div>
@@ -29,20 +28,24 @@ class vehicles {
         <div class="card-body">
           <div class="card-title text-center h5">${this.model}</div><br>
           <hr>
-          <div class="card-text">${this.kilometers}</div>
+          <div class="card-text">${this.kilometers} km</div>
           <div class="card-text">numberOfSeats Type: ${this.numberOfSeats}  </div>
           <div class="card-text">Fuel Type: ${this.fuelType}  </div>
           <div class="card-text">PS: ${this.PS}  </div>
 
         </div>
-        <div class="card-footer bg-transparent text-center">
-        <div class="card-text">Price: ${this.price}  </div>
-          
-          </div>
+        <p>
+            <button class="btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePricecars"
+                aria-expanded="false" aria-controls="collapsePricecars">Show Price
+            </button>
+        </p>
+        <div class="collapse" id="collapsePricecars">
+            <div class="card card-body">Price: ${this.price}</div>
+        </div>
     </div> `;
   }
 }
-let ente = new vehicles("#", "Citroën", "2CV", 1988, 99000, 7000, 40, 4, "normal");
+let ente = new vehicles("./02advanced/vehicles/2CV.jpg", "Citroën", "2CV", 1988, 99000, 7000, 40, 4, "normal");
 class trucks extends vehicles {
   bLicenceSuff: boolean;
   bodyType: string;
@@ -56,32 +59,40 @@ class trucks extends vehicles {
     this.emptyWeight = emptyWeight
   }
   truckInfo() {
-    return
-    `    <div class="card">
-        <div class="card-header d-flex justify-content-between bg-transparent">
-          <div>${this.model} by ${this.maker} </div>
+    return  `    <div class=" card">
+    <div class="card-header d-flex justify-content-between bg-transparent">
+        ${this.model} by ${this.maker}
+    </div>
+    <img src="${this.image}" class="card-img-top img-fluid" alt="...">
+    <div class="card-body">
+        <div class="card-title text-center h5">${this.model}<br> ${this.bodyType}
         </div>
-        <img src="${this.image}" class="card-img-top img-fluid" alt="...">
-        <div class="card-body">
-          <div class="card-title text-center h5">${this.model}<br> ${this.bodyType} </div><br>
-          <hr>
-          <div class="card-text">${this.kilometers}</div>
-          <div class="card-text">B License enough: ${this.bLicenceSuff}<br>
-          Empty Weight: ${this.emptyWeight}<br>
-          Max Load Weight: ${this.maxLoadWeight} </div>
-          <div class="card-text">numberOfSeats Type: ${this.numberOfSeats}  </div>
-          <div class="card-text">Fuel Type: ${this.fuelType}  </div>
-          <div class="card-text">PS: ${this.PS}  </div>
+        <hr>
+        <div class="card-text">
+            ${this.kilometers} km <br>
+            B License enough: ${this.bLicenceSuff}<br>
+            Empty Weight: ${this.emptyWeight}<br>
+            Max Load Weight: ${this.maxLoadWeight} <br>
+            Seats: ${this.numberOfSeats}
+            Fuel Type: ${this.fuelType} <br>
+            PS: ${this.PS}
+        </div>
 
+
+        <p>
+            <button class="btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePriceTrucks"
+                aria-expanded="false" aria-controls="collapsePriceTrucks">Show Price
+            </button>
+        </p>
+        <div class="collapse" id="collapsePriceTrucks">
+            <div class="card card-body">Price: ${this.price}</div>
         </div>
-        <div class="card-footer bg-transparent text-center">
-        <div class="card-text">Price: ${this.price}  </div>
-          
-          </div>
-    </div> `;
+    </div>
+             `;
   }
 }
-let ram = new trucks("#", "Chrysler", "Dodge Ram", 2022, 0, 54000, 308, 6, "normal", true, "Pickup Quad-Cab", 8000, 34000);
+// <div class="card-footer bg-transparent text-center"> (for line 77) </div> (for line 76)
+let ram = new trucks("./02advanced/vehicles/RAM.JPG", "Chrysler", "Dodge Ram", 2022, 0, 54000, 308, 6, "normal", true, "Pickup Quad-Cab", 8000, 34000);
 class motorBikes extends vehicles {
   ageLimit: boolean;
   canDriveWithB: boolean;
@@ -91,8 +102,7 @@ class motorBikes extends vehicles {
     this.canDriveWithB = canDriveWithB;
   }
   bikeInfo() {
-    return
-    `    <div class="card">
+    return `    <div class="card">
         <div class="card-header d-flex justify-content-between bg-transparent">
           <div>${this.model} by ${this.maker} </div>
         </div>
@@ -100,7 +110,7 @@ class motorBikes extends vehicles {
         <div class="card-body">
           <div class="card-title text-center h5">${this.model}</div><br>
           <hr>
-          <div class="card-text">${this.kilometers}</div>
+          <div class="card-text">${this.kilometers} km </div>
           <div class="card-text">Can drive with B: ${this.canDriveWithB}  </div>
           <div class="card-text">Has age limit: ${this.ageLimit}  </div>
 
@@ -108,15 +118,28 @@ class motorBikes extends vehicles {
           <div class="card-text">PS: ${this.PS}  </div>
 
         </div>
-        <div class="card-footer bg-transparent text-center">
-        <div class="card-text">Price: ${this.price}  </div>
+        <p>
+            <button class="btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePriceBike"
+                aria-expanded="false" aria-controls="collapsePriceBike">Show Price
+            </button>
+        </p>
+        <div class="collapse" id="collapsePriceBike">
+            <div class="card card-body">Price: ${this.price}</div>
+        </div>
           
           </div>
     </div> `;
   }
+  
 }
 
-let mofaMaxi = new motorBikes("#", "Steyr Daimler Puch", "N2", 1992, 20000, 10000, 1.2, 1, "normal", false, true)
-
-var outputcars = (document.querySelector(("ouput-cars") as HTMLElement))
-outputcars?.innerHTML = vehicles.carInfo();
+let mofaMaxi = new motorBikes("./02advanced/vehicles/n2.jpg", "Steyr Daimler Puch", "N2", 1992, 20000, 10000, 1.2, 1, "normal", false, true);
+// output cards
+var outputEnte = (document.getElementById("ouput-cars") as HTMLElement);
+outputEnte.innerHTML = ente.carInfo();
+// output trucks
+var outputRam =(document.getElementById("ouput-trucks") as HTMLElement);
+outputRam.innerHTML += ram.truckInfo();
+// output bikes
+var outputPuch = document.getElementById("ouput-bikes") as HTMLElement;
+outputPuch.innerHTML  += mofaMaxi.bikeInfo();
